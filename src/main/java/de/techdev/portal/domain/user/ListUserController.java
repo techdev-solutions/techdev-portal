3a52d0230b04142a98adaf47e18c0f02a5c21114
Moritz Schulze <moritz.schulze@techdev.de>
2015-06-04 22:36:24 +0200

    TRACKR-274 - Add new employee Refactored the user loading from the database into a repository and also loaded the roles along.

--- src/main/java/de/techdev/portal/domain/user/ListUserController.java
+++ src/main/java/de/techdev/portal/domain/user/ListUserController.java
@@ -1,35 +1,23 @@
 package de.techdev.portal.domain.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-import javax.sql.DataSource;
-
-import java.util.List;
-
-import static java.util.Arrays.asList;
-
 @Controller
 public class ListUserController {
 
     @Autowired
-    private DataSource dataSource;
+    private UserRepository userRepository;
 
     @RequestMapping(value = "/listUsers", method = RequestMethod.GET)
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public ModelAndView listUsers() {
         ModelAndView modelAndView = new ModelAndView("domain/users/listUsers");
-        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
-        List<User> users = jdbcTemplate.query("SELECT username, enabled FROM users", (rs, rowNum) ->
-                        new User(rs.getString("username"), "EMPTY", rs.getBoolean("enabled"), true, true, true, asList())
-        );
-        modelAndView.addObject("users", users);
+        modelAndView.addObject("users", userRepository.findAll());
         return modelAndView;
     }
 }
