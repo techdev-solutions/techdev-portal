9651dcdca86ad3260f2ff1be3dde95cb890e001c
Moritz Schulze <moritz.schulze@techdev.de>
2015-06-04 15:05:22 +0200

    TRACKR-274 - Add new employee Added a page that lists all users and is only visitable by admins Extracted the logout button into a fragemtn

--- src/main/java/de/techdev/portal/domain/LandingPageController.java
+++ src/main/java/de/techdev/portal/domain/LandingPageController.java
@@ -4,16 +4,12 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
-import java.security.Principal;
-
 @Controller
 public class LandingPageController {
 
     @RequestMapping("/landing")
-    public ModelAndView showLandingPage(Principal principal) {
-        ModelAndView modelAndView = new ModelAndView("landingPage");
-        modelAndView.addObject("userName", principal.getName());
-        return modelAndView;
+    public ModelAndView showLandingPage() {
+        return new ModelAndView("landingPage");
     }
 
 }
