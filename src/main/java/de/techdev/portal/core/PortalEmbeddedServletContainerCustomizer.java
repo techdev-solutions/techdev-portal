54dc9bd51c0ac6bad8ac15658fc83c6ce642addc
Moritz Schulze <moritz.schulze@techdev.de>
2015-06-05 11:02:32 +0200

    TRACKR-274 - Add new employee Added custom 403 error page.

---
+++ src/main/java/de/techdev/portal/core/PortalEmbeddedServletContainerCustomizer.java
@@ -0,0 +1,14 @@
+package de.techdev.portal.core;
+
+import org.springframework.boot.context.embedded.ConfigurableEmbeddedServletContainer;
+import org.springframework.boot.context.embedded.EmbeddedServletContainerCustomizer;
+import org.springframework.boot.context.embedded.ErrorPage;
+import org.springframework.http.HttpStatus;
+
+public class PortalEmbeddedServletContainerCustomizer implements EmbeddedServletContainerCustomizer {
+
+    @Override
+    public void customize(ConfigurableEmbeddedServletContainer container) {
+        container.addErrorPages(new ErrorPage(HttpStatus.FORBIDDEN, "/403"));
+    }
+}
